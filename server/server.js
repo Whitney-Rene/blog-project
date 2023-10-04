0194bf47f6d516d1bbe0c49824812c5c67a489b4
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-04 00:18:17 -0400

    adds first route to server.js

--- server/server.js
+++ server/server.js
@@ -6,3 +6,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 1012;
 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json('You can do hard things, Whitney-Rene')
+})
+
+app.listen(PORT, () => {
+    console.log(`Estoy escuchando en ${PORT}`)
+})

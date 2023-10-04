b27d1b4bcdf95bb1e6b5e65fbe45902f20ca593c
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-04 00:35:40 -0400

    adds fetch request to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+//a string?
+const apiUrl = 'http://localhost:1012';
 
 function App() {
 
+  const [note, setNote] = useState('');
+
+  const fetchNote = async () => {
+    const response = await fetch(`${apiUrl}`);
+    const data = await response.json();
+    setNote(data);
+  }
+
+  useEffect(() => {
+    fetchNote();
+  }, []);
+
   return (
     <>
-    <h1>Hello Whitney-Rene</h1>
+    <h1>{note}</h1>
     </>
   )
 }

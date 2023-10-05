dbe41ff21d3c6e1100c2de7e5b477d4b38c009c4
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-05 14:27:08 -0400

    installs/imports  react-modal

--- client/src/components/ViewBlogPreview.jsx
+++ client/src/components/ViewBlogPreview.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import { apiUrl } from "../utils";
 import { formatTime } from "../utils";
+import ReactModal from 'react-modal';
 import '../App.css';
-import ViewIndividualBlogModal from "./ViewEntireBlog";
+import ViewEntireBlog from "./ViewEntireBlog";
+
+if (process.env.NODE_ENV !== 'test') ReactModal.setAppElement('#root');
 
 function ViewBlogPreview ()  {
 
     const [previewData, setPreviewData] = useState([]);
-    //all infor for a blog
+    
 
    //could I put this in the utils.js file?  so I can call it in CreateBlogPost?
     const blogsPreviewData = async () => {
@@ -17,6 +20,7 @@ function ViewBlogPreview ()  {
                 throw new Error ("Failed to fetch preview data for blogs");
             }
             const data = await response.json();
+            console.log(data);
             setPreviewData(data);
         } catch (error) {
             console.error('Error loading contacts')
@@ -35,7 +39,7 @@ function ViewBlogPreview ()  {
             <div>
             {previewData.map((item) => (
                 
-            <div className="blogPreview" key={`${item.blog_id}-${item.author_id}`}>
+                <div className="blogPreview" key={`${item.blog_id}-${item.author_id}`}>
                     <img className="images" src={`${item.blog_picture}`} alt={`${item.blog_title}`}/>
                     <br />
                     <a href="#" className="blog-title" onClick=''>{item.blog_title}</a >
--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.get('/blogpreview', async (req, res) => {
     // console.log(res);
     try{
         //destructuring assignment syntax  db.query=operation resp for executing sql queryies against db and retunr the result
-        const { rows : blogpreview } = await db.query('SELECT blogpost.blog_picture, blogpost.blog_title, blogpost.blog_publishdate, blogpost.blog_id, authors.author_name, authors.author_id FROM blogpost INNER JOIN authors ON blogpost.author_id = authors.author_id');
+        const { rows : blogpreview } = await db.query('SELECT blogpost.blog_content, blogpost.blog_picture, blogpost.blog_title, blogpost.blog_publishdate, blogpost.blog_id, authors.author_name, authors.author_id FROM blogpost INNER JOIN authors ON blogpost.author_id = authors.author_id');
         //result of query is an object, contains properties=one is a rows property, row property contains an array of rows as a result of sql query
         res.send(blogpreview);
         // console.log(blogpreview);
